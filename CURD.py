0b6522d1b5f84cf07d26b517bb80cb59ee900f4a
Muhammad Abdullah <whello544@gmail.com>
2026-06-27 22:54:15 +0500

    Implement GET route to fetch post by ID
    Added error handling for retrieving posts by ID.

--- CURD.py
+++ CURD.py
@@ -47,3 +47,23 @@ def create_posts(post : Post):
     post_dict ['id'] = randrange(0,100000)
     my_post.append(post_dict)
     return{"data" : post_dict}
+
+# error handling
+
+# Add HTTPException to your fastapi imports at the top:
+from fastapi import FastAPI, status, HTTPException
+
+# Add this helper function below your my_post array
+def find_post(id):
+    for p in my_post:
+        if p["id"] == id:
+            return p
+
+# Add this GET route at the bottom
+@app.get("/posts/{id}")
+def get_post(id : int):
+    post = find_post(int(id))
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"post with id : {id} was not found")
+    return {"post_detail" : post}
