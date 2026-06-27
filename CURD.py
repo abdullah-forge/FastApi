4e0ef305abc41410b95854cf45ab28f1e34fbaf2
Muhammad Abdullah <whello544@gmail.com>
2026-06-27 22:53:37 +0500

    Implement POST route and data validation for posts
    Added data validation and a POST route for creating posts.

--- CURD.py
+++ CURD.py
@@ -25,3 +25,25 @@ def root():
 @app.get("/posts")
 def get_post():
     return{"data" : my_post}
+
+# Data validation and create post
+
+# Add these to your imports at the top:
+from typing import Optional
+from random import randrange
+from fastapi import status
+
+# Add this schema below app = FastAPI()
+class Post(BaseModel):
+    title : str
+    content : str
+    published : bool = True
+    rating : Optional[int] = None
+
+# Add this POST route at the bottom
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def create_posts(post : Post):
+    post_dict = post.dict()
+    post_dict ['id'] = randrange(0,100000)
+    my_post.append(post_dict)
+    return{"data" : post_dict}
