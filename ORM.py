18bae3e6381b44045c31c9111ae48c69ee929c4d
Muhammad Abdullah <whello544@gmail.com>
2026-06-30 13:24:04 +0500

    Add Post model to ORM
    Added Post model to ORM with fields for title, content, published status, and created timestamp.

--- ORM.py
+++ ORM.py
@@ -16,3 +16,19 @@ def get_db():
         yield db
     finally:
         db.close()
+
+#models.py
+
+from .database import Base
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.sql.expression import text
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+    published = Column(Boolean, server_default='True')
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
