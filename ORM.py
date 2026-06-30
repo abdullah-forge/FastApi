a3a439a11995cae7b75dc8acb4e8bd558210609f
Muhammad Abdullah <whello544@gmail.com>
2026-06-30 13:25:12 +0500

    Initialize FastAPI app and create database tables
    Added FastAPI app initialization and database setup.

--- ORM.py
+++ ORM.py
@@ -45,3 +45,14 @@ class PostBase(BaseModel):
 
 class PostCreate(PostBase):
     pass
+
+#main.py
+
+from fastapi import FastAPI, status, HTTPException, Response, Depends
+from sqlalchemy.orm import Session
+from . import models, schemas
+from .database import engine, get_db
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
